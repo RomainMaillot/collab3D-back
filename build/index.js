b0a12f36dbff8709ae4807281e4269de39a2e7be
RomainMaillot <romainmaillot.rm@gmail.com>
2021-05-17 16:03:32 +0200

    change origin

--- build/index.js
+++ build/index.js
@@ -14,7 +14,7 @@ const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server, {
     cors: {
-        origin: '*:*',
+        origin: '*',
         methods: ["GET", "POST"]
     }
 });
